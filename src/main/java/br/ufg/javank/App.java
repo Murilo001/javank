b1c2caae589a4f1226bfc9ea3590fa0e3a156be5
Murilo Oliveira <murilo001os@gmail.com>
2020-03-16 21:09:32 -0300

    Initial commit, project created

---
+++ src/main/java/br/ufg/javank/App.java
@@ -0,0 +1,13 @@
+package br.ufg.javank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

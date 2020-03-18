3eb5ee8af66db0f4c1e25407c01e8df88202c422
Murilo Oliveira <murilo001os@gmail.com>
2020-03-17 23:41:10 -0300

    Add methods of get and create accounts to clients superclass

--- src/main/java/br/ufg/client/Client.java
+++ src/main/java/br/ufg/client/Client.java
@@ -1,9 +1,35 @@
 package br.ufg.client;
 
+import java.util.ArrayList;
+import java.util.List;
+import br.ufg.account.*;
+
 public class Client {
 	private int id;
-	
+
+	private List<Account> accounts = new ArrayList<Account>();
+
 	public int getId() {
 		return this.id;
+	}
+
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public boolean createAccount(AccountType type) {
+		Account clientNewAccount;
+		switch (type) {
+		case SPECIAL:
+			clientNewAccount = new Special();
+			break;
+		case SAVING:
+			clientNewAccount = new Saving();
+			break;
+		default:
+			return false;
+		}
+		this.accounts.add(clientNewAccount);
+		return true;
 	}
 }

a4e8aae52537dc805a8cf50a2f4de6021b46aada
Murilo Oliveira <murilo001os@gmail.com>
2020-03-17 23:42:08 -0300

    Add main class script to utilize deposit, withdraw, transfer and get balance method of accounts.

--- src/main/java/br/ufg/javank/App.java
+++ src/main/java/br/ufg/javank/App.java
@@ -1,13 +1,32 @@
 package br.ufg.javank;
 
+import br.ufg.client.*;
+import br.ufg.account.*;
+
 /**
- * Hello world!
+ * 
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "Welcome to Javank, your best and only bank choose!" );
+    	JuridicPerson google = new JuridicPerson();
+    	JuridicPerson microsoft = new JuridicPerson();
+    	PhysicalPerson adolfo = new PhysicalPerson();
+    	PhysicalPerson rodolfo = new PhysicalPerson();
+    	google.createAccount(AccountType.SPECIAL);
+    	google.createAccount(AccountType.SAVING);
+    	microsoft.createAccount(AccountType.SPECIAL);
+    	adolfo.createAccount(AccountType.SAVING);
+    	rodolfo.createAccount(AccountType.SPECIAL);
+    	
+    	Account googleFirstAccount = google.getAccounts().get(0);
+    	googleFirstAccount.deposit(980.98);
+    	googleFirstAccount.withDraw(500.08);
+    	googleFirstAccount.transfer(220, google.getAccounts().get(1));
+        System.out.printf("Final balance: $ %.2f", googleFirstAccount.getBalance());
+        System.out.println( "Thank you!" );
     }
 }
